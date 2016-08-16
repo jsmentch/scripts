e59ae4eb5e84372f9a4cb3203923b8290fd7ab6a
jsmentch <jsmentch@gmail.com>
2016-08-16 16:46:16 -0400

    updated script

--- convertbin.sh
+++ convertbin.sh
@@ -5,11 +5,12 @@ echo "The script seems to be working."
 ls *.hdr | while read l;
 	do
 		fsl5.0-fslchfiletype NIFTI_GZ "$l"
-		echo "$l" converted to NIFTI
+#		echo "$l" converted to NIFTI
 	done
 
-ls *.nii.gz | while read ni;
+ls *.nii.gz | while read ni
 	do
-		fsl5.0-fslmaths "$ni" -bin "$ni"_bin.nii.gz
-		echo "$ni" binarized
+		fn=$(basename "$ni" ".nii.gz")
+		fsl5.0-fslmaths $fn.nii.gz -bin bin_$fn.nii.gz
 	done
+
