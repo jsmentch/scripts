5f32e3b0e1927efc973e75ca78a6f507b8cdd920
jsmentch <jsmentch@gmail.com>
2016-08-17 14:47:37 -0400

    fiexed log with >>s

--- convertbin.sh
+++ convertbin.sh
@@ -7,15 +7,16 @@ echo "ROI conversion log" > log.txt
 ls *.hdr | while read l;
 	do
 		fsl5.0-fslchfiletype NIFTI_GZ "$l"
-		echo "$l converted to NIFTI at $date" > log.txt
+		echo "$l converted to NIFTI at $date" >> log.txt
 	done
 
 ls *.nii.gz | while read n;
 	do
 		fsl5.0-fslmaths $n -bin $n
-		echo "$n binarized at $date" > log.txt
+		echo "$n binarized at $date" >> log.txt
 		fsl5.0-flirt -in $n -ref ~/Documents/JeffOpenFMRI_Task002_Pandora/templates/grpbold7Tp1/from_mni/avg152T1_brain.nii.gz -out $n -init ~/Documents/JeffOpenFMRI_Task002_Pandoras/templates/grpbold7Tp1/xfm/mni2tmpl_12dof.mat -applyxfm
-		echo "$n transformed at $date" > log.txt
+		echo "$n transformed at $date" >> log.txt
 		fsl5.0-fslmaths $n -bin $n
-		echo "$n binarized at $date" > log.txt
+		echo "$n binarized at $date" >> log.txt
+
 	done
